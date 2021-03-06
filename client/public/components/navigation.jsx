0d068a555f5a7b920e8d54f21d8ac903bf59ed73
Jessica Barnett <jessba2@pdx.edu>
2021-03-06 13:11:26 -0800

    added skeleton files for Navigation and Footer

---
+++ client/public/components/navigation.jsx
@@ -0,0 +1,14 @@
+/*All the jsx files start with line below */
+import React from "react";
+
+/* Structure of function */
+function Navigation() {
+	return(
+        <div className="navigation">
+
+        </div>
+    )
+}
+
+/* always ends with */
+export  default Navigation;
---
+++ client/public/footer.jsx
@@ -0,0 +1,14 @@
+/*All the jsx files start with line below */
+import React from "react";
+
+/* Structure of function */
+function Footer() {
+	return(
+        <div className="footer">
+
+        </div>
+    )
+}
+
+/* always ends with */
+export  default Footer;
