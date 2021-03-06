12bddbbe76af6d10fa7f245dbfcc67b81f04ae38
Brita Budnick <brita.budnick@gmail.com>
2021-03-05 18:15:27 -0800

    Successfully render json string on React frontend - ugly but effective

--- api/routes/testAPI.js
+++ api/routes/testAPI.js
@@ -2,6 +2,8 @@ var express = require("express");
 var router = express.Router(); 
 
 router.get("/", function(req, res, next) {
+    res.send("trying some stuff");
+    
     res.send("API SUCCESS");
 });
 
