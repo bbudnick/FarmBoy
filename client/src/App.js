f3d9affb4ca83015ca2f33bfd9a05b380a063933
Jessica Barnett <jessba2@pdx.edu>
2021-03-06 00:31:34 -0800

    remove routes code

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,6 @@
 import React, { Component } from "react";
 import './App.css';
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
-import { Navigation, Footer, Home, About, Contact } from "./components";
+
 
 class App extends Component {
   constructor(props) {
@@ -25,18 +24,7 @@ class App extends Component {
       <div className= "App">    
       <div className="App">
       <h1 className="App-title">WHAT'S UP PLANTY PEOPLE</h1>
-        <Router>
-          <Navigation />
-            <Switch>
-              <Route path="/" exact component={() => <Home />} />
-              <Route path="/about" exact component={() => <About />} />
-              <Route path="/contact" exact component={() => <Contact />} />
-            </Switch>
-            <Footer />
-        </Router>
       </div>
-        
-    
         <p className="App-intro">{this.state.apiResponse}</p>
       </div>
     );
