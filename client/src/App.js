86564374599ee4a7b7c1b08d3f01eba405724112
Jessica Barnett <jessba2@pdx.edu>
2021-03-06 12:28:17 -0800

    added code to App.js for route definitions and paths

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,9 @@
 import React, { Component } from "react";
 import './App.css';
-
+/* to define router from the react-router-dom installed */
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom"
+/* where to find pages routes go to */
+import { Navigation, Footer, Home, Native}
 
 class App extends Component {
   constructor(props) {
@@ -24,6 +27,20 @@ class App extends Component {
       <div className= "App">    
       <div className="App">
       <h1 className="App-title">WHAT'S UP PLANTY PEOPLE</h1>
+      </div>
+      <div>
+        <Router>
+            <Navigation />
+               <Switch> 
+                  <Route path = "/" exact component {() => <Home />} />
+                  <Route path = "/Native Plants" exact component {() => <HNative Plants />} />
+                  <Route path = "/Backyard Habitat" exact component {() => <Backyard Habitat />} />
+                  <Route path = "/Weather" exact component {() => <Weather/>} />
+                  <Route path = "/Purpose" exact component {() => <Purpose />} />
+                  <Route path = "/Contact" exact component {() => <Contact />} />
+                  <Route path = "/Weather" exact component {() => <Weather/>} />
+               </Switch>
+        </Router>
       </div>
         <p className="App-intro">{this.state.apiResponse}</p>
       </div>
