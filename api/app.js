a677fc473307f0cbf4df0b363f25b6e6c39c1402
Brita Budnick <brita.budnick@gmail.com>
2021-03-08 20:23:30 -0800

    Added coconut trefle to experiment with key values for table render

--- api/app.js
+++ api/app.js
@@ -9,6 +9,8 @@ var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var testAPIRouter = require('./routes/testAPI');
 var weatherRouter = require('./routes/weatherAPI');
+var trefleRouter = require('./routes/trefleAPI');
+var trefleCoconuts = require('./routes/trefleCoconut');
 
 var app = express();
 
@@ -27,6 +29,8 @@ app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/testAPI', testAPIRouter);
 app.use('/weatherAPI', weatherRouter);
+app.use('/trefleAPI', trefleRouter);
+app.use('/trefleCoconut', trefleCoconuts);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
---
+++ api/routes/trefleCoconut.js
@@ -0,0 +1,44 @@
+const express = require("express");
+const fetch = require("node-fetch");
+var router = express.Router();
+
+router.get("/", function (req, res, next) {
+  //parameters for the POST request
+  //from the trefle website, provides a token that times out and
+  //assuages the CORS error
+  const params = {
+    origin: "http://localhost:9000",
+    token: "lMjyhDCKH5D43P-4SlgHJeM7557Ch37abJ0TBT0-gBE",
+  };
+
+  const urlPlants = "https://trefle.io/api/auth/claim";
+
+  let fetchPlants = async () => {
+    try {
+      let response = await fetch(urlPlants, {
+        method: "post",
+        body: JSON.stringify(params),
+        headers: { "Content-Type": "application/json" },
+      });
+      const json = await response.json();
+      fetchAllPlants("https://trefle.io/api/v1/species/search?q=coconut&token=" + json.token);
+    } catch (error) {
+      console.log("Request failed", error);
+    }
+  };
+
+  let fetchAllPlants = async (url) => {
+    try {
+      let response = await fetch(url);
+      let plantData = await response.json();
+      let stringyPlant = JSON.stringify(plantData);
+      res.send(stringyPlant);
+    } catch (error) {
+      console.log("Request failed", error);
+    }
+  };
+
+  fetchPlants();
+});
+
+module.exports = router;
