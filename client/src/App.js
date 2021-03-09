97f241cd7ee1927dc65b9bc7dc5f2868e4ebdcd2
Brita Budnick <brita.budnick@gmail.com>
2021-03-09 14:17:52 -0800

    Still a mess but progress

--- client/src/App.js
+++ client/src/App.js
@@ -1,18 +1,16 @@
 import React, { Component } from "react";
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
 class App extends Component {
   constructor(props) {
     super(props);
-    this.state = { apiResponse: ""};
+    this.state = { apiResponse: "" };
   }
-
   callAPI() {
-    fetch("http://localhost:9000/weatherAPI")
-    .then(res => res.text())
-    .then(res => this.setState({ apiResponse: res }))
-    .catch(err => err);
+    fetch("http://localhost:9000/trefleCoconut")
+      .then((res) => res.text())
+      .then((res) => this.setState({ apiResponse: res }))
+      .catch((err) => err);
   }
 
   componentDidMount() {
@@ -21,13 +19,11 @@ class App extends Component {
 
   render() {
     return (
-      <div className= "App">
+      <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">WHAT'S UP PLANTY PEOPLE</h1>
+          <h1 className="App-title">Farm boy, fetch me that?</h1>
+          <p className="App">{this.state.apiResponse}</p>
         </header>
-        <p className="App-intro">{this.state.apiResponse}</p>
-
       </div>
     );
   }
