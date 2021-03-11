45c0a9749f3a0958b9ee0f98d98b4ffa43e8d36a
Brita Budnick <brita.budnick@gmail.com>
2021-03-11 14:00:00 -0800

    Successfully render images from trefle in table booya

--- api/routes/trefleAPI.js
+++ api/routes/trefleAPI.js
@@ -25,10 +25,6 @@ router.get("/", function (req, res, next) {
     try {
       let response = await fetch(url);
       let plantData = await response.json();
-      /*for (let [key, value] of Object.plantData(data)) {
-          console.log(`${key}: ${value}`);
-      }
-*/
       jade.compile('trefle');
       res.render('trefle', { title: 'Plant Data', plants: plantData});
     } catch (error) {
