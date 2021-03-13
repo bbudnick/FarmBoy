eadd39a588020b9bac32fd77dff0e137d02cd384
Brita Budnick <brita.budnick@gmail.com>
2021-03-13 13:03:23 -0800

    Reworking weather so it's more clear

--- api/routes/weatherAPI.js
+++ api/routes/weatherAPI.js
@@ -16,15 +16,19 @@ router.get("/", function (req, res, next) {
   var weather = require("openweather-apis");
   weather.setLang("en");
   weather.setCityId("5746545");
+  weather.setCoordinate("45.52173612944446, -122.66915933968288");
+  weather.setCity("Portland");
+  weather.setZipCode("97214");
   weather.setUnits("imperial");
   weather.setAPPID("5ee69f6d165fd55cf78232e0d1a348ea");
 
   try {
-    weather.getSmartJSON(function (err, smart) {
-      res.render("weather", { title: "Weather Data", forecast: smart });
+    weather.getAllWeather(function (err, JSONObj) {
+      res.render("weather", { forecast: JSONObj });
     });
   } catch (error) {
     console.log("Request failed", error);
   }
 });
+
 module.exports = router;
